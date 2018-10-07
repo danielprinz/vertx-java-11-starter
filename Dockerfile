bf15c579468c51771a792ec96b7004f8d22273ce
danielprinz <daniel.prinz@outlook.com>
2018-10-07 12:38:07 +0200

    configure async log4j2 logging

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM openjdk:11-jre
 
 ARG JAR_FILE
-ENTRYPOINT ["/usr/bin/java", "-jar", "/usr/share/app/starter-service.jar"]
+ENTRYPOINT ["/usr/bin/java", "-Dlog4j2.contextSelector=org.apache.logging.log4j.core.async.AsyncLoggerContextSelector", "-jar", "/usr/share/app/starter-service.jar"]
 
 ADD target/${JAR_FILE} /usr/share/app/starter-service.jar
--- src/main/java/com/danielprinz/vertx/java11/MainVerticle.java
+++ src/main/java/com/danielprinz/vertx/java11/MainVerticle.java
@@ -1,20 +1,25 @@
 package com.danielprinz.vertx.java11;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
 
 public class MainVerticle extends AbstractVerticle {
 
+  private static final Logger LOG = LoggerFactory.getLogger(MainVerticle.class);
+
   @Override
   public void start(Future<Void> startFuture) throws Exception {
-    vertx.createHttpServer().requestHandler(req -> {
+    vertx.createHttpServer().requestHandler(req ->
       req.response()
         .putHeader("content-type", "text/plain")
-        .end("Hello from Vert.x!");
-    }).listen(8080, http -> {
+        .end("Hello from Vert.x!")
+    ).listen(8080, http -> {
       if (http.succeeded()) {
         startFuture.complete();
-        System.out.println("HTTP server started on http://localhost:8080");
+        LOG.info("HTTP server started on http://localhost:8080");
       } else {
         startFuture.fail(http.cause());
       }
